efff27cccb367aefac310af6ed7ecd2b1efbd195
ravihlb <ravi@deliverydireto.com.br>
2021-06-28 20:27:03 -0300

    Create Articles Model, Controller & index, view templates

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,16 @@
+<?php
+// src/Controller/ArticlesController.php
+
+namespace App\Controller;
+
+class ArticlesController extends AppController {
+    public function index() {
+        $articles = $this->Articles->find()->all();
+        $this->set(compact('articles'));
+    }
+
+    public function view($id = null) {
+        $article = $this->Articles->get($id);
+        $this->set(compact('article'));
+    }
+}
