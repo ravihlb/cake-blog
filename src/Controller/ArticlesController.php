e6af103d8e615ab90ba896a8b23a04d91afb9ac2
ravihlb <ravi@deliverydireto.com.br>
2021-07-05 18:27:33 -0300

    Add function for adding articles

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -3,14 +3,34 @@
 
 namespace App\Controller;
 
+use App\Controller\AppController;
+
 class ArticlesController extends AppController {
+    public function initialize(): void {
+        parent::initialize();
+
+        $this->loadComponent('Flash');
+    }
+    
     public function index() {
         $articles = $this->Articles->find()->all();
-        $this->set(compact('articles'));
     }
 
     public function view($id = null) {
         $article = $this->Articles->get($id);
         $this->set(compact('article'));
+    }
+
+    public function add() {
+        $article = $this->Articles->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add your article.'));
+        }
+        $this->set('article', $article);
     }
 }
