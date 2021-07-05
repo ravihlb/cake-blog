11bd20ab70e598d6698cc299fb57f39fa2fb681d
ravihlb <ravi@deliverydireto.com.br>
2021-07-05 19:04:10 -0300

    Edit articles

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -20,7 +20,7 @@ class ArticlesController extends AppController {
         $article = $this->Articles->get($id);
         $this->set(compact('article'));
     }
-
+    
     public function add() {
         $article = $this->Articles->newEmptyEntity();
         if ($this->request->is('post')) {
@@ -31,6 +31,20 @@ class ArticlesController extends AppController {
             }
             $this->Flash->error(__('Unable to add your article.'));
         }
+        $this->set('article', $article);
+    }
+    
+    public function edit($id = null) {
+        $article = $this->Articles->get($id);
+        if ($this->request->is(['post', 'put'])) {
+            $this->Articles->patchEntity($article, $this->request->getData());
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been updated.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to update your article.'));
+        }
+
         $this->set('article', $article);
     }
 }
--- templates/Articles/index.php
+++ templates/Articles/index.php
@@ -20,6 +20,9 @@
     <td>
         <?= $article->created->format(DATE_RFC850) ?>
     </td>
+    <td>
+        <?= $this->Html->link('Edit', ['action' => 'edit', $article->id]) ?>
+    </td>
 </tr>
 <?php endforeach; ?>
 </table>
